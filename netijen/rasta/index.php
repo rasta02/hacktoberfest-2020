c6e8432ba43604c05ba29f560543a75e01068224
greenhacking02 <31425219+greenhacking02@users.noreply.github.com>
2019-10-01 22:59:20 +0700

    nitip ya bro

---
+++ netijen/rasta/index.php
@@ -0,0 +1,29 @@
+<!--Simple Page Defacement By 4LM05TH3V!L Copyright © 2019 Hargai Karya Orang Lain,Jangan Hapus Copyright-->
+<html xml:lang="en" lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<title>Owned By 4LM05TH3V!L</title>
+<link rel="icon" type="image/x-icon" href="https://i.ibb.co/88tPzg9/Skull-Full-art-1920x1080-HD-and-FREE-Stock-Photo-wallpaper-wp6409436-picsay.jpg">
+<link href="https://fonts.googleapis.com/css?family=Rajdhani" rel="stylesheet">
+<meta name="description" content="Hidden Ghost Team">
+<meta name="keywords" content="Hidden Ghost Team">
+<meta http-equiv="cache-control" content="index,cache">
+<meta http-equiv="pragma" content="index,cache">
+<meta name="theme-color" content="black">
+<div style="height: auto; min-height: 100%;">
+<div style="text-align: center; width:800px; margin-left: -400px; position: absolute; top: 30%; left: 50%;">
+<body bgcolor="black">
+<img src="https://i.ibb.co/88tPzg9/Skull-Full-art-1920x1080-HD-and-FREE-Stock-Photo-wallpaper-wp6409436-picsay.jpg" alt="IMG-20190210-WA0015-picsay">
+<font color="white" size="6" face="Rajdhani">
+<br>
+<i>
+Owned By 4LM05TH3V!L
+<br>
+<font size="3">
+<br>
+| Shootz |
+<br>
+M1NT_1X - EppCrazy - Mr.TR1N1TY - Mr.Chip21 - MamaHMudA - Mr.ReposadoCyber - Doraemon v1.5 - Mr.xBarakuda 
+<br>
+Desktop77N3T - Cyberpunks - ./MrTahuSumedang - ./C3W3KBerB4T4n9 - limit[ed] - Mr.b0t4k - TN72 - Syntax7
+<br>
+<br>
+<a style="color:white;" href="mailto:almostheevil@gmail.com">Contact Me!</a>
